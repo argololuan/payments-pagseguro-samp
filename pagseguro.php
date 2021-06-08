0a43f7253bded54ecfac07c6edf272dd4d40d0f7
Luan Argolo <ipsluan@icloud.com>
2021-06-07 21:39:43 -0300

    Update code

--- pagseguro.php
+++ pagseguro.php
@@ -2,8 +2,8 @@
     if(isset($_GET['code']))
     {
 	$code = $_GET['code'];
-	$email = "ipsluan@icloud.com";
-	$token = " ";
+	$email = "YOUR_EMAIL";
+	$token = "YOUR_TOKEN";
 	print file_get_contents("https://ws.pagseguro.uol.com.br/v2/transactions/".$code."?email=".$email."&token=".$token);
     }
 ?>
